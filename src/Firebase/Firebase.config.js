a6e15769ae40f7778f9d62a99e12228064f92c3f
mdtanvir037 <hossainmdjabed037@gmail.com>
2021-10-29 01:51:21 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCucXGO7MgROBCv9CKZQ2h07i8WkGA6GnM",
+  authDomain: "tourism-589c8.firebaseapp.com",
+  projectId: "tourism-589c8",
+  storageBucket: "tourism-589c8.appspot.com",
+  messagingSenderId: "215395846768",
+  appId: "1:215395846768:web:fa2c0674b7194dae74c2b5",
+};
+
+export default firebaseConfig;
